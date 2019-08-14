878756f8b2dbcc9e20c39bc0ca6bb0470d588c47
elugas123 <jesusugas16@gmail.com>
2019-08-14 13:08:00 -0300

    first commit

---
+++ ninja.model/Manager/InvoiceManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ninja.model.Entity;
+using ninja.model.Mock;
+
+namespace ninja.model.Manager {
+
+    public class InvoiceManager {
+
+        private InvoiceMock _mock;
+
+        public InvoiceManager() {
+
+            this._mock = InvoiceMock.GetInstance();
+
+        }
+
+        public IList<Invoice> GetAll() {
+
+            return this._mock.GetAll();
+
+        }
+
+        public Invoice GetById(long id) {
+
+            return this._mock.GetById(id);
+
+        }
+
+        public void Insert(Invoice item) {
+
+            this._mock.Insert(item);
+
+        }
+
+        public void Delete(long id)
+        {
+            Invoice invoice = this.GetById(id);
+            this._mock.Delete(invoice);
+        }
+
+        public Boolean Exists(long id)
+        {
+            return this._mock.Exists(id);
+        }
+
+        public void AddDetail(long id, IList<InvoiceDetail> detail)
+        {
+            this._mock.AddDetail(id, detail);
+        }
+
+        public void UpdateDetail(InvoiceDetail item)
+        { 
+            this._mock.UpdateDetail(item);
+        }
+
+        public void Update(Invoice item)
+        {
+            this._mock.Update(item);
+        }
+
+        public void UpdateDetail(long id, IList<InvoiceDetail> detail)
+        {
+            this._mock.DeleteDetails(id);
+            this._mock.AddDetail(id, detail);
+        }
+
+        public void DeleteDetail(InvoiceDetail item)
+        {
+            this._mock.DeleteDetail(item);
+        }
+
+        public void AddInvoiceDetail(InvoiceDetail item)
+        {
+            this._mock.AddInvoiceDetail(item);
+        }
+    }
+}
---
+++ ninja/Controllers/InvoiceController.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ninja.model.Manager;
+using ninja.model.Entity;
+
+namespace ninja.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        InvoiceManager manager = new InvoiceManager();
+        
+        public ActionResult Index()
+        {
+            return View(manager.GetAll());
+        }
+
+        public ActionResult Detail(long Id)
+        {
+            ViewBag.IdInvoice = Id;
+            return View(manager.GetById(Id).GetDetail());
+        }
+
+        public ActionResult CreateInvoice()
+        {
+            return View();
+        }
+
+        public ActionResult NewInvoice(Invoice item)
+        {
+            if (item != null)
+                manager.Insert(item);
+            return Redirect("Index");
+        }
+
+        public ActionResult Delete(long Id)
+        {
+            manager.Delete(Id);
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        public ActionResult DeleteDetail(long Id, long InvoiceId)
+        {
+            manager.DeleteDetail(manager.GetById(InvoiceId).GetDetail().First(x => x.Id == Id));
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        public ActionResult Update(long Id)
+        {
+            return View(manager.GetById(Id));
+        }
+
+        public ActionResult UpdateInvoice(Invoice item)
+        {
+            manager.Update(item);
+            return Redirect("Index");
+        }
+
+        public ActionResult UpdateAddDetail(long Id, long InvoiceId)
+        {
+            return View(manager.GetById(InvoiceId).GetDetail().First(x => x.Id == Id));
+        }
+
+        public ActionResult UpdateDetail(InvoiceDetail item)
+        {
+            manager.UpdateDetail(item);
+            return RedirectToAction("Detail", new { Id = item.InvoiceId});
+        }
+
+        public ActionResult AddDetail(long Id)
+        {
+            return View(new InvoiceDetail() { InvoiceId = Id });
+        }
+
+        public ActionResult AddInvoiceDetail(InvoiceDetail item)
+        {
+            manager.AddInvoiceDetail(item);
+            return RedirectToAction("Detail", new { Id = item.InvoiceId });
+        }
+    }
+}
+
+
